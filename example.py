373d4f048761d207a9e75a6993153745cda014b6
ggszk <gengo.suzuki@gmail.com>
2020-07-29 14:08:42 +0900

    bug fix: table or column names can include period

--- example.py
+++ example.py
@@ -8,7 +8,9 @@ samples = [
     "SELECT jenn FROM Person AS jenn WHERE jenn.name = 'Jennifer'",
     "SELECT company.name FROM Person AS p, p.WORKS_FOR AS company WHERE p.name = 'Jennifer'",
     "SELECT Person, Person.name FROM Person",
-    "SELECT from_node.n_id, dijkstra(from_node, to_node, 'CONNECT_TO', 'cost'), to_node.n_id FROM g2 AS from_node, g2 AS to_node WHERE from_node.n_id = 0 AND to_node.n_id = 8"
+    "SELECT from_node.n_id, dijkstra(from_node, to_node, 'CONNECT_TO', 'cost'), to_node.n_id FROM g2 AS from_node, g2 AS to_node WHERE from_node.n_id = 0 AND to_node.n_id = 8",
+    "SELECT add(col1, col2) FROM table3",
+    "SELECT p1.name FROM Person AS p1",
 ]
 
 rule = open('partiql_grammer.lark').read()
